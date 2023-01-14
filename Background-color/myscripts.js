6fa525d34b91a775375b016e9f590ba0c04d6910
bhargav2905 <107643007+bhargav2905@users.noreply.github.com>
2023-01-14 17:41:30 +0530

    Add files via upload
    lets upgrade project

---
+++ Background-color/myscripts.js
@@ -0,0 +1,12 @@
+
+const h4 = document.getElementById('color');
+
+h4.style.color = "red";
+
+h4.style.fontSize = "50px";
+h4.style.padding = "20px";
+
+function taskForClick() {
+  pp.style.backgroundColor = 'red';
+}
+change.addEventListener('click', taskForClick);
